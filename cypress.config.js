e0b73e48c3d0f2aad85fb0f0149fa351432ad353
demoproject708 <darshan3tester@gmail.com>
2025-01-16 14:22:47 +0530

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
+
+module.exports = defineConfig({
+  e2e: {"video": true,
+  "videoCompression": 32,
+  "videoUploadOnPasses": false,
+  "videosFolder": "cypress/videos",
+  
+    // Specify the pattern for .feature files
+    specPattern: "**/*.feature",
+    pageLoadTimeout: 20000,
+    
+    // Set up event listeners for preprocessor and reporter
+    setupNodeEvents(on, config) {
+      // Enable cucumber preprocessor
+      on("file:preprocessor", cucumber());
+    },
+    
+    // Configure reporter
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports", // Directory to store reports
+      overwrite: true,             // Overwrite existing reports
+      html: true,                  // Generate an HTML report
+      json: true,                  // Generate a JSON report
+    },
+  },
+});
